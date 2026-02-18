51420f197aef9e1b51a504c61f32c10f73920580
safiurrahman677-del <safiurrahman677@gmail.com>
2026-02-18 11:27:39 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "help-nuri-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./nuri.png",
+  "./me.png",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
